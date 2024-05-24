8b32975b603d66e1dd67a39724ec52ef691a3343
TheLobbyPigeon <gijsvanvugt2002@gmail.com>
2024-05-24 22:06:11 +0200

    Kleine aanpassing HistoryActivity

--- app/src/main/java/com/jochemtb/gezinsgericht/GUI/HistoryActivity.java
+++ app/src/main/java/com/jochemtb/gezinsgericht/GUI/HistoryActivity.java
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.jochemtb.gezinsgericht.R;
 
-public class HistoryAcvitiy extends AppCompatActivity {
+public class HistoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
