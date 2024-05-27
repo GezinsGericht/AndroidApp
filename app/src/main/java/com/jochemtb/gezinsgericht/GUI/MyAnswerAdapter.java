a0a4c52623efcfbc78a5c2661f06ba179500667f
TheLobbyPigeon <gijsvanvugt2002@gmail.com>
2024-05-27 17:47:20 +0200

    myanswer page updated

--- app/src/main/java/com/jochemtb/gezinsgericht/GUI/MyAnswerAdapter.java
+++ app/src/main/java/com/jochemtb/gezinsgericht/GUI/MyAnswerAdapter.java
@@ -3,6 +3,7 @@ package com.jochemtb.gezinsgericht.GUI;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,23 +16,25 @@ import java.util.List;
 public class MyAnswerAdapter extends RecyclerView.Adapter<MyAnswerAdapter.MyAnswerViewHolder> {
 
     private final List<String[]> answers;
+    private final List<String[]> questions;
 
-    public MyAnswerAdapter(List<String[]> answers) {
+    public MyAnswerAdapter(List<String[]> answers, List<String[]> questions) {
         this.answers = answers;
+        this.questions = questions;
     }
 
     @NonNull
     @Override
     public MyAnswerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.question_item, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
         return new MyAnswerViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyAnswerViewHolder holder, int position) {
-        String[] answer = answers.get(position);
-        holder.questionAnswered.setText(answer[0]);
-        holder.answerGiven.setText(answer[1]);
+        String[] questionArray = questions.get(position);
+        String[] answerArray = answers.get(position);
+        holder.bind(questionArray, answerArray);
     }
 
     @Override
@@ -40,13 +43,26 @@ public class MyAnswerAdapter extends RecyclerView.Adapter<MyAnswerAdapter.MyAnsw
     }
 
     public static class MyAnswerViewHolder extends RecyclerView.ViewHolder {
-        TextView questionAnswered;
-        TextView answerGiven;
+        private final LinearLayout container;
 
         public MyAnswerViewHolder(@NonNull View itemView) {
             super(itemView);
-            questionAnswered = itemView.findViewById(R.id.question_answered);
-            answerGiven = itemView.findViewById(R.id.answer_given);
+            container = itemView.findViewById(R.id.container);  // Correctly reference the container LinearLayout
+        }
+
+        public void bind(String[] questions, String[] answers) {
+            container.removeAllViews();  // Clear previous views
+
+            for (int i = 0; i < questions.length; i++) {
+                View view = LayoutInflater.from(container.getContext()).inflate(R.layout.question_item, container, false);
+                TextView questionView = view.findViewById(R.id.question_answered);
+                TextView answerView = view.findViewById(R.id.answer_given);
+
+                questionView.setText(questions[i]);
+                answerView.setText(answers[i]);
+
+                container.addView(view);
+            }
         }
     }
 }
