5e4f6f6344965166bf654b4c15500ef3305eb491
TheLobbyPigeon <gijsvanvugt2002@gmail.com>
2024-05-28 14:18:31 +0200

    question en Quiz aangemaakt

---
+++ app/src/main/java/com/jochemtb/gezinsgericht/domain/Quiz.java
@@ -0,0 +1,29 @@
+package com.jochemtb.gezinsgericht.domain;
+
+import java.util.ArrayList;
+
+public class Quiz {
+    private ArrayList<Question> questionList;
+    private int totalQuestions;
+
+    public ArrayList<Question> getQuestionList() {
+        return questionList;
+    }
+
+    public void setQuestionList(ArrayList<Question> questionList) {
+        this.questionList = questionList;
+    }
+
+    public int getTotalQuestions() {
+        return totalQuestions;
+    }
+
+    public void setTotalQuestions(int totalQuestions) {
+        this.totalQuestions = totalQuestions;
+    }
+
+    public Quiz(ArrayList<Question> questionList, int totalQuestions) {
+        this.questionList = questionList;
+        this.totalQuestions = totalQuestions;
+    }
+}
