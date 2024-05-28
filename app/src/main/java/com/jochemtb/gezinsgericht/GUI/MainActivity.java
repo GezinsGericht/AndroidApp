02425ff355b5bce00372cd7659d381e84a57d8de
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-05-28 11:45:12 +0200

    Single responsibility update
    + Created LineChartHelper class
    -/+ Changed mainActivity, so it is single responsibility
    -/+ Made loginActivity start screen

--- app/src/main/java/com/jochemtb/gezinsgericht/GUI/MainActivity.java
+++ app/src/main/java/com/jochemtb/gezinsgericht/GUI/MainActivity.java
@@ -20,6 +20,7 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.jochemtb.gezinsgericht.R;
+import com.jochemtb.gezinsgericht.domain.LineChartHelper;
 
 import java.sql.Array;
 import java.util.ArrayList;
@@ -31,9 +32,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView usernameTv;
     private CheckBox checkbox_1, checkbox_2, checkbox_3, checkbox_4, checkbox_5, checkbox_6, checkbox_7;
     private Button navbar_2, navbar_3;
-    private LineChart progressionChart;
-
+    private LineChartHelper lineChartHelper;
     private final String LOG_TAG = "HomepageActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,20 +44,6 @@ public class MainActivity extends AppCompatActivity {
         buildChart();
 
         usernameTv.setText("Sietse 't Hooft"); // Dummie data
-
-        navbar_2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, HistoryActivity.class));
-            }
-        });
-
-        navbar_3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, QuizActivity.class));
-            }
-        });
     }
 
     private void initViewComponents() {
@@ -70,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         checkbox_6 = findViewById(R.id.CB_homepage_6);
         checkbox_7 = findViewById(R.id.CB_homepage_7);
 
-        progressionChart = findViewById(R.id.chart_homepage);
+        lineChartHelper = new LineChartHelper(findViewById(R.id.chart_homepage));
 
         navbar_2 = findViewById(R.id.BTN_navbar2);
         navbar_3 = findViewById(R.id.BTN_navbar3);
@@ -78,49 +65,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void buildChart(){
-        //Makes description that is visible above the chart
-        Description description = new Description();
-        description.setText("Vooruitgang patiënt");
-        description.setPosition(250f, 15f);
-        description.setTextSize(10f);
-        progressionChart.setDescription(description);
-        progressionChart.getAxisRight().setDrawLabels(false);
-
-        //Makes the x-as
-        XAxis xAxis = progressionChart.getXAxis();
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setValueFormatter(new IndexAxisValueFormatter());
-        xAxis.setLabelCount(4);
-        xAxis.setGranularity(1f);
-
-        //Makes the y-as
-        YAxis yAxis = progressionChart.getAxisLeft();
-        yAxis.setAxisMinimum(0f);
-        yAxis.setAxisMaximum(15f);
-        yAxis.setAxisLineWidth(2f);
-        yAxis.setAxisLineColor(Color.BLACK);
-        yAxis.setLabelCount(10);
 
         //Makes the entries (points)
         List<Entry> entries = new ArrayList<>();
+
+        //Dummy data:
         entries.add(new Entry(0, 0f));
         entries.add(new Entry(1, 3f));
         entries.add(new Entry(2, 9f));
         entries.add(new Entry(3, 7f));
         entries.add(new Entry(4, 13f));
 
+        //Add the entries to the lineChart
+        lineChartHelper.addEntries(entries);
+
         //Makes the line(s)
-        LineDataSet dataSet = new LineDataSet(entries, "vooruitgang");
-        dataSet.setColor(Color.BLUE);
-        dataSet.setLineWidth(5f);
+        lineChartHelper.addDataSet("vooruitgang", Color.BLUE, 5f);
 
-        LineData lineData = new LineData(dataSet);
-
-        progressionChart.setData(lineData);
-        progressionChart.invalidate();
     }
 
     private void navToSession(){
+        navbar_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HistoryActivity.class));
+            }
+        });
+
         navbar_3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
---
+++ app/src/main/java/com/jochemtb/gezinsgericht/domain/LineChartHelper.java
@@ -0,0 +1,92 @@
+package com.jochemtb.gezinsgericht.domain;
+
+import android.graphics.Color;
+import android.util.Log;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class LineChartHelper {
+    private LineChart lineChart;
+    private Description description;
+    private List<Entry> entries;
+    private LineDataSet dataSet;
+    private final String LOG_TAG = "LineChartHelper";
+
+    public LineChartHelper(LineChart lineChart){
+        this.lineChart = lineChart;
+        this.description = new Description();
+        this.entries = new ArrayList<>();
+        initLineChart();
+    }
+
+    // This method makes the chart (without the dataLine)
+    private void initLineChart(){
+        makeDiscription();
+        lineChart.setDescription(description);
+        lineChart.getAxisRight().setDrawLabels(false);
+
+        makeXaxis();
+        makeYaxis();
+        Log.d(LOG_TAG, "initLineChart done");
+    }
+
+    // This method makes the description
+    private void makeDiscription(){
+        description.setText("Vooruitgangsgrafiek"); // Perhaps make this the string which checkbox is used? (Arraylist or hashmap?)
+        description.setPosition(250f, 15f);
+        description.setTextSize(10f);
+    }
+
+    // This method makes the X-as of the chart
+    private void makeXaxis(){
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter());
+        xAxis.setLabelCount(4);
+        xAxis.setGranularity(1f);
+    }
+
+    // This method makes the Y-as of the chart
+    private void makeYaxis(){
+        YAxis yAxis = lineChart.getAxisLeft();
+        yAxis.setAxisMinimum(0f);
+        yAxis.setAxisMaximum(15f);
+        yAxis.setAxisLineWidth(2f);
+        yAxis.setAxisLineColor(Color.BLACK);
+        yAxis.setLabelCount(10);
+    }
+
+    // This method adds the entries send by the mainActivity
+    public void addEntries(List<Entry> entries){
+        this.entries = entries;
+        Log.d(LOG_TAG, "Entries added" + this.entries);
+    }
+
+    // This method creates the line in the chart, with the extra options send by the mainActivity
+    public void addDataSet(String label, int color, float width){
+        dataSet = new LineDataSet(entries, label);
+        dataSet.setColor(color);
+        dataSet.setLineWidth(width);
+        Log.d(LOG_TAG, "Dataset added");
+        makeLine();
+    }
+
+    // This method prints the line on the chart
+    private void makeLine(){
+        LineData lineData = new LineData(dataSet);
+        lineChart.setData(lineData);
+        lineChart.invalidate();
+        Log.d(LOG_TAG, "Dataline added");
+    }
+}
