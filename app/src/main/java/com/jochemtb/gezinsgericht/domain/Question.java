3279b8da629eec86e862089d86f5fe17bf89e891
TheLobbyPigeon <gijsvanvugt2002@gmail.com>
2024-05-29 15:08:38 +0200

    habitat verwijderd uit Question

--- app/src/main/java/com/jochemtb/gezinsgericht/domain/Question.java
+++ app/src/main/java/com/jochemtb/gezinsgericht/domain/Question.java
@@ -4,7 +4,6 @@ public class Question {
     private int questionid;
     private String question;
     private int weight;
-//    private Habitat habitat;
     private String answer1;
     private String answer2;
     private String answer3;
@@ -45,15 +44,7 @@ public class Question {
     public void setWeight(int weight) {
         this.weight = weight;
     }
-
-//    public Habitat getHabitat() {
-//        return habitat;
-//    }
-
-//    public void setHabitat(Habitat habitat) {
-//        this.habitat = habitat;
-//    }
-
+    
     public String getAnswer1() {
         return answer1;
     }
@@ -94,14 +85,4 @@ public class Question {
         this.answer5 = answer5;
     }
 
-//    public Question(String question, int weight, Habitat habitat, String answer1, String answer2, String answer3, String answer4, String answer5) {
-//        this.question = question;
-//        this.weight = weight;
-//        this.habitat = habitat;
-//        this.answer1 = answer1;
-//        this.answer2 = answer2;
-//        this.answer3 = answer3;
-//        this.answer4 = answer4;
-//        this.answer5 = answer5;
-//    }
 }
