33a537ff2eb9fa8c7e46f5805003076310d02c97
JanRoelofs <janroe2004@gmail.com>
2024-05-31 10:42:56 +0200

    ApiService opgehaald

--- app/src/main/java/com/jochemtb/gezinsgericht/API/ApiService.java
+++ app/src/main/java/com/jochemtb/gezinsgericht/API/ApiService.java
@@ -1,4 +1,22 @@
 package com.jochemtb.gezinsgericht.API;
 
+
+import com.jochemtb.gezinsgericht.API.Login.ForgotPasswordRequest;
+import com.jochemtb.gezinsgericht.API.Login.ForgotPasswordResponse;
+import com.jochemtb.gezinsgericht.API.Login.LoginRequest;
+import com.jochemtb.gezinsgericht.API.Login.LoginResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
 public interface ApiService {
+    @Headers("Content-Type: application/json")
+    @POST("login")
+    Call<LoginResponse> loginUser(@Body LoginRequest loginRequest);
+
+    @Headers("Content-Type: application/json")
+    @POST("login/reset")
+    Call<ForgotPasswordResponse> forgotPassword(@Body ForgotPasswordRequest forgotPasswordRequest);
 }
