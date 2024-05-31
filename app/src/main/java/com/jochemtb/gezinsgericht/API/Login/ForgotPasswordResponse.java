0550172f33a732a4de4ff2041ec1a32e686f4a6b
JanRoelofs <janroe2004@gmail.com>
2024-05-31 10:48:51 +0200

    Classes toevoegen

--- app/src/main/java/com/jochemtb/gezinsgericht/API/Login/ForgotPasswordResponse.java
+++ app/src/main/java/com/jochemtb/gezinsgericht/API/Login/ForgotPasswordResponse.java
@@ -1,4 +1,14 @@
 package com.jochemtb.gezinsgericht.API.Login;
 
 public class ForgotPasswordResponse {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    // Setter
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
