6f34822158aaeeb7fd22fa94e11e6b7678528279
TheLobbyPigeon <gijsvanvugt2002@gmail.com>
2024-06-03 12:21:05 +0200

    Squashed commit of the following:
    commit 531401836c31aa8c0118e07aab42c6b0d14c6717
    Author: TheLobbyPigeon <gijsvanvugt2002@gmail.com>
    Date:   Mon Jun 3 12:20:02 2024 +0200
    
        hotfix Koppleing ViewId
    
    commit 88faf790f3b004546a9fb4cc2f7a3e4b7906806c
    Author: TheLobbyPigeon <gijsvanvugt2002@gmail.com>
    Date:   Mon Jun 3 12:14:06 2024 +0200
    
        Squashed commit of the following:
    
        commit 0e6b82c7b846efe01c09a25ccb8447aee0e63217
        Author: Jochem-tb <jochem.welp@gmail.com>
        Date:   Fri May 31 16:23:05 2024 +0200
    
            Automatic login when token valid
    
        commit a839d72e66743b784ac5500d9968de05a0046757
        Author: Jochem-tb <jochem.welp@gmail.com>
        Date:   Wed May 29 13:54:50 2024 +0200
    
            Wachtwoord Vergeten Funcitoneel + 5min max
    
            5min max op het versturen van mailtje & verwanderen wachtwoord.
    
        commit 7d824824c69e44ad1c1c0ae24bfb1e6ff2b5c997
        Author: Jochem-tb <jochem.welp@gmail.com>
        Date:   Tue May 28 16:29:00 2024 +0200
    
            Creation API, login system funcitonal
    
            Login super super traag..... Geen idee hoe, maar het werkt.

--- app/src/main/java/com/jochemtb/gezinsgericht/GUI/ActivationActivity.java
+++ app/src/main/java/com/jochemtb/gezinsgericht/GUI/ActivationActivity.java
@@ -1,28 +1,53 @@
 package com.jochemtb.gezinsgericht.GUI;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.jochemtb.gezinsgericht.R;
 
+import com.jochemtb.gezinsgericht.repository.UserRepository;
+
 public class ActivationActivity extends AppCompatActivity {
 
     Button confirmButton;
+    private UserRepository userRepository;
+    SharedPreferences sharefPref;
+    private static final String RESET_TOKEN = "resetToken";
+    private static final String LOG_TAG = "ActivationActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        userRepository = new UserRepository(this);
+        sharefPref = getSharedPreferences("sharedPref", MODE_PRIVATE);
+        Log.d(LOG_TAG, "onCreate: resetToken="+sharefPref.getLong(RESET_TOKEN, 0)+", isFiveMinutesPassed="+isFiveMinutesPassed(sharefPref.getLong(RESET_TOKEN, 0)));
+        if(isFiveMinutesPassed(sharefPref.getLong(RESET_TOKEN, 0))){
+            Toast.makeText(this, "Activation link has expired, please request a new one.", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(ActivationActivity.this, LoginActivity.class));
+        }
         setContentView(R.layout.activity_activation);
-        confirmButton = findViewById(R.id.BT_activation_submit);
+        confirmButton = findViewById(R.id.BT_login_submit);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                userRepository.updatePassword()
                 startActivity(new Intent(ActivationActivity.this, LoginActivity.class));
             }
         });
+    }
+
+    public static boolean isFiveMinutesPassed(long timestampInSeconds) {
+        long currentTimeInSeconds = System.currentTimeMillis() / 1000; // Current time in seconds
+        long fiveMinutesInSeconds = 5 * 60; // 5 minutes in seconds
+
+        // Check if the difference between current time and the provided timestamp is greater than 5 minutes
+        return (currentTimeInSeconds - timestampInSeconds) >= fiveMinutesInSeconds;
     }
 }
--- app/src/main/java/com/jochemtb/gezinsgericht/GUI/MainActivity.java
+++ app/src/main/java/com/jochemtb/gezinsgericht/GUI/MainActivity.java
@@ -3,13 +3,16 @@ package com.jochemtb.gezinsgericht.GUI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.graphics.Color;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Description;
@@ -19,6 +22,7 @@ import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.google.longrunning.WaitOperationRequest;
 import com.jochemtb.gezinsgericht.R;
 import com.jochemtb.gezinsgericht.domain.LineChartHelper;
 
@@ -33,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox checkbox_1, checkbox_2, checkbox_3, checkbox_4, checkbox_5, checkbox_6, checkbox_7;
     private Button navbar_2, navbar_3;
     private LineChartHelper lineChartHelper;
+    private ImageView settingsLogo;
+    private LineChart progressionChart;
+    private SharedPreferences sharedPref;
     private final String LOG_TAG = "HomepageActivity";
 
     @Override
@@ -42,6 +49,10 @@ public class MainActivity extends AppCompatActivity {
         initViewComponents();
         navToSession();
         buildChart();
+
+        //TESTING
+        sharedPref = getSharedPreferences("sharedPref", MODE_PRIVATE);
+        Toast.makeText(this, sharedPref.getString("jwtToken", "Error"), Toast.LENGTH_LONG).show();
 
         usernameTv.setText("Sietse 't Hooft"); // Dummie data
     }
@@ -58,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
         checkbox_7 = findViewById(R.id.CB_homepage_7);
 
         lineChartHelper = new LineChartHelper(findViewById(R.id.chart_homepage));
+        settingsLogo = findViewById(R.id.IV_main_settings);
 
         navbar_2 = findViewById(R.id.BTN_navbar2);
         navbar_3 = findViewById(R.id.BTN_navbar3);
@@ -85,6 +97,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void navToSession(){
+        settingsLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Log.i(LOG_TAG, "Uitloggen");
+                    sharedPref.edit().remove("jwtToken").apply();
+                    Toast.makeText(getBaseContext(), "Uitloggen succesvol", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "Error: " + e.getMessage());
+                }
+            }
+        });
+
         navbar_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -99,4 +125,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }
