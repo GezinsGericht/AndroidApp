2ffaad602450a007a4a101d4f84bf2f995274590
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-06-04 13:03:40 +0200

    Creation of GoalsActivity

---
+++ app/src/main/java/com/jochemtb/gezinsgericht/GUI/GoalsActivity.java
@@ -0,0 +1,13 @@
+package com.jochemtb.gezinsgericht.GUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.jochemtb.gezinsgericht.R;
+
+public class GoalsActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goals);
+    }
+}
